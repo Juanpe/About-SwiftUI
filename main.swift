4087e198434af1ee2d39e61f1fcd9a4cce0c0538
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-06-11 13:11:52 +0530

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,1 +1,15 @@
 // TO-DO
+import SwiftUI
+
+struct Task: Equatable, Hashable, Codable, Identifiable {
+  let id: UUID
+  var title: String
+  var isDone: Bool
+
+  init(title: String, isDone: Bool) {
+    self.id = UUID()
+    self.title = title
+    self.isDone = isDone
+  }
+}
+
